db1fb2f249a1852cae46bb3158b8c60f6e2b528a
RobsonFe <robson12ferreira@gmail.com>
2025-03-09 22:31:14 -0300

    atualizando api

--- src/main/java/br/com/robson/tasks/TasksApplication.java
+++ src/main/java/br/com/robson/tasks/TasksApplication.java
@@ -9,6 +9,7 @@ public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 		System.out.println("Acesse a API localmente em http://localhost:8080");
+		System.out.println("Acesse a Documentação em http://localhost:8080/swagger-ui/index.html");
 	}
 
 }
